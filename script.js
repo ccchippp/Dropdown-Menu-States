30536477ae53e1e51f787277f55c2c99c5e53d33
ccchippp <clempke90@gmail.com>
2021-06-16 22:03:31 -0400

    removed unnecessary lines

--- script.js
+++ script.js
@@ -5,5 +5,3 @@ const selectElements = document.querySelectorAll('data-custom')
 selectElements.forEach(selectElement => {
     new Select(selectElement)
 })
-
-const select = new Select(selectElement)
