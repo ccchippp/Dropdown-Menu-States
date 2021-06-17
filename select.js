30f37a13cd591846463c3f6cf701293da0b1309f
ccchippp <clempke90@gmail.com>
2021-06-16 22:10:16 -0400

    create elements

--- select.js
+++ select.js
@@ -1,5 +1,8 @@
 export default class Select {
     constructor(element) {
         this.element = element
+        this.customElement = document.createElement('div')
+        this.labelElement = document.createElement('span')
+        this.optionsCustomElement = document.createElement('ul')
     }
 }
